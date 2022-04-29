623d0738842ae871ee1193c3849765f15e967e3b
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-04-30 01:57:29 +0900

    add upload function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { NextPage } from "next";
+import { Auth } from "aws-amplify";
+import { CognitoUser } from "@aws-amplify/auth";
 
 const Home: NextPage = () => {
+  // useEffect(() => {
+  //   (async function () {
+  //     const user: CognitoUser = await Auth.signIn({
+  //       username: "joseph95501@gmail.com",
+  //       password: "joseph2002",
+  //     });
+  //     console.log(
+  //       user.getUserAttributes((err, result) => {
+  //         if (err) {
+  //           console.log(err);
+  //         }
+  //         console.log(result);
+  //       })
+  //     );
+  //   })();
+  // }, []);
   return <>Hello World!</>;
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    './src/**/*.tsx',
+    './**/*.tsx',
   ],
   theme: {
     extend: {},
